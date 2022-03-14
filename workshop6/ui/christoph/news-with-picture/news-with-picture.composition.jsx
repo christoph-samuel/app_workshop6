a820a8756bf28f485ce86c15dc2b37828c23303f
Christoph Samuel <chsamuel@gmx.at>
2022-03-14 17:02:50 +0100

    v1.0.0: basic design of component

--- workshop6/ui/christoph/news-with-picture/news-with-picture.composition.jsx
+++ workshop6/ui/christoph/news-with-picture/news-with-picture.composition.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { NewsWithPicture } from './news-with-picture';
+import {NewsWithPicture} from './news-with-picture';
 
 export const BasicNewsWithPicture = () => (
-  <NewsWithPicture text="hello from NewsWithPicture" />
+    <NewsWithPicture text="hello from NewsWithPicture"
+                     imgSrc="https://static.bit.dev/homepage-bit/reuse-page/cloud-components.png"/>
 );
--- workshop6/ui/christoph/news-with-picture/news-with-picture.jsx
+++ workshop6/ui/christoph/news-with-picture/news-with-picture.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-export function NewsWithPicture({ text }) {
-  return (
-    <div>
-      {text}
-    </div>
-  );
+export function NewsWithPicture({text, imgSrc}) {
+    const styleDiv = {
+        border: '1px solid black',
+        borderRadius: '5px',
+        width: 'max-content',
+        height: 'max-content',
+        padding: '10px',
+        margin: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+    const styleImg = {width: "400px"}
+
+    return (
+        <div style={styleDiv}>
+            <h1>{text}</h1>
+            <img alt="Image" src={imgSrc} style={styleImg}/>
+        </div>
+    );
 }
