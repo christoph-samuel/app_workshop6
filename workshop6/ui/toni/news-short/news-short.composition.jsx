ba6929aa4536db7bbe807110944b05fc06909acb
Christoph Samuel <chsamuel@gmx.at>
2022-03-16 13:39:53 +0100

    v1.0.0: basic design of component

--- workshop6/ui/toni/news-short/news-short.composition.jsx
+++ workshop6/ui/toni/news-short/news-short.composition.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { NewsShort } from './news-short';
+import {NewsShort} from './news-short';
 
 export const BasicNewsShort = () => (
-  <NewsShort text="hello from NewsShort" />
+    <NewsShort
+        heading="Einheitliche Tiebreak-Regel bei Grand-Slam-Turnieren"
+        text="Die Veranstalter der vier großen Tennis-Grand-Slam-Turniere – Australian Open in Melbourne, French Open in Paris, Wimbledon in London und US Open in New York – haben sich auf eine einheitliche Tiebreak-Regel geeinigt."
+    />
 );
